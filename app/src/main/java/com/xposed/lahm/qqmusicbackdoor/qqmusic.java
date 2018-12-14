cc95791e681d8bceef4d801bf707f17612aa3085
lahm <l292538769@gmail.com>
2018-12-14 17:09:26 +0800

    first commit

---
+++ app/src/main/java/com/xposed/lahm/qqmusicbackdoor/qqmusic.java
@@ -0,0 +1,39 @@
+package com.xposed.lahm.qqmusicbackdoor;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+/**
+ * Project Name:QQMusicBackdoor
+ * Package Name:com.xposed.lahm.qqmusicbackdoor
+ * Created by lahm on 2018/12/14 5:06 PM .
+ */
+public class qqmusic implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.contains("com.tencent.qqmusic")) {
+            try {
+                XposedBridge.hookAllMethods(findClass("com.tencent.qqmusic.activity.IdentifyingCodeActivity", loadPackageParam.classLoader),
+                        "c", new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                super.beforeHookedMethod(param);
+
+                            }
+
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                super.afterHookedMethod(param);
+                                param.setResult(true);
+                            }
+                        });
+            } catch (Exception e) {
+                XposedBridge.log("fucku--Exception");
+            }
+        }
+    }
+}
